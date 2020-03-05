ac6abd1a56dc4d683b9907ab6c2a45606feb3249
sonam <sonam.tmg96@gmail.com>
2020-03-05 14:43:15 -0500

    [UI] Added Tag class, Tags relation, tag and untag methods for PictureController and initializeTag Method.

---
+++ app/models/Tag.java
@@ -0,0 +1,40 @@
+package models;
+
+import io.ebean.Finder;
+import io.ebean.Model;
+import play.data.validation.Constraints;
+
+import javax.persistence.Id;
+
+public class Tag extends Model {
+
+    @Id
+    private int tagId;
+
+    @Constraints.Required
+    private String tagContent;
+
+    public Tag(int tagId, @Constraints.Required String tagContent) {
+        this.tagId = tagId;
+        this.tagContent = tagContent;
+    }
+
+    public static Finder<Long, Tag> find = new Finder<>(Tag.class);
+    public static Tag findByTagContent(String tagContent) {return find.byId(parsers.StringParsers.parseLong(tagContent));}
+
+    public int getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(int tagId) {
+        this.tagId = tagId;
+    }
+
+    public String getTagContent() {
+        return tagContent;
+    }
+
+    public void setTagContent(String tagContent) {
+        this.tagContent = tagContent;
+    }
+}
